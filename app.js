cdceb59dfdd9cd273655701780cd35b37d8d25d1
maragathaa <maragathaa2005@gmail.com>
2024-11-24 23:01:50 +0530

    connected to mongodb

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Import Express
 const express = require('express');
 
+// Using Node.js `require()`
+const mongoose = require('mongoose');
+
 // Initialize the app
 const app = express();
 
@@ -26,7 +29,16 @@ app.post('/post_test',(req,res)=>
     res.send("Hello world");
 });
 
+//connect to the mongodb
+mongoose.connect('mongodb://127.0.0.1:27017/test') //mongodb://localhost:27017- connected ..if value changes -not connected
+//similar to try and catch block
+  .then(() => {console.log('Connected!');
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+    
+  }).catch(()=>console.log('connection error'));
+
+
+
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
